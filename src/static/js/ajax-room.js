f6dacc3af583270c396ba9efd76854e47d01d8ee
Islom <islom.uzbekboy@gmail.com>
2018-04-23 14:41:42 +0500

    Istiklol svg is ready

--- src/static/js/ajax-room.js
+++ src/static/js/ajax-room.js
@@ -1,103 +1,199 @@
 $(document).ready(function () {
-
     $(".data").click(function () {
-
+        $(".loader1").fadeOut();
         $("#room-scheme").empty();
         $(".count").text("0");
 
         var instance = $(".data").attr("data-instance");
         var hall = $(".data").attr("data-hall");
 
-        $.getJSON("../../room.json", function (data) {
+        $("#room-scheme").load('../../istiklol.svg', function () {
+            $("path").click(function () {
+                $("#room-scheme").children().hide();
+                $.getJSON("../../room.json", function (data) {
 
-            if(data.info) {
-                $("#imgAction").attr("src", data.info.poster);
+                    if(data.info) {
+                        $("#imgAction").attr("src", data.info.poster);
 
-                var dayAction = new Date(data.info.date);
+                        var dayAction = new Date(data.info.date);
 
-                var timeAction = dayAction.getHours();
-                var minuteAction = dayAction.getMinutes();
-                console.log(data.info.date);
-                console.log(timeAction + " : " + minuteAction);
-                $("#nameAction").text(data.info.title);
-                $("#whereAction").text(data.info.hall);
-                $("#timeAction").text(timeAction + " : " + minuteAction);
-            }
+                        var timeAction = dayAction.getHours();
+                        var minuteAction = dayAction.getMinutes();
+                        console.log(data.info.date);
+                        console.log(timeAction + " : " + minuteAction);
+                        $("#nameAction").text(data.info.title);
+                        $("#whereAction").text(data.info.hall);
+                        $("#timeAction").text(timeAction + " : " + minuteAction);
+                    }
 
-            $.each(data, function (key, value) {
-                console.log(value.length);
-                $("#room-scheme").append("<span style='position: relative; top: -15px; right: 10px'>ryad: "+(key+1)+"</span>");
-                for(var v in value) {
-                    //console.log(value[v].place + " in row " + value[v].row);
+                    $.each(data, function (key, value) {
+                        console.log(value.length);
+                        $("#room-scheme").append("<span style='position: relative; top: -15px; right: 10px'>ryad: "+(key+1)+"</span>");
+                        for(var v in value) {
+                            //console.log(value[v].place + " in row " + value[v].row);
 
-                    var svg = $("<svg height=\"20\" width=\"20\"> <circle cx=\"10\" cy=\"10\" r=\"5\" stroke=\"black\" stroke-width=\"1\" fill=\"#31D61E\"/> </svg>");
-                    svg.attr("id", "row_"+value[v].row+"_"+value[v].place);
-                    $("#room-scheme").append(svg);
-
-
-                     $("#row_" + value[v].row + "_" + value[v].place).find('circle').attr({
-                         "data-price": value[v].price,
-                         "data-status": value[v].status,
-                         "data-id": value[v].id,
-                         "data-place": value[v].place,
-                         "data-row": value[v].row,
-                         "data-tipso": "Цена: " + value[v].price +"" + "\nМесто: "+value[v].place
-
-                     });
-
-                }
-
-                $("#room-scheme").append("<br/>");
-
-            });
-            $('.loader1').fadeOut();
+                            var svg = $("<svg height=\"20\" width=\"20\"> <circle cx=\"10\" cy=\"10\" r=\"5\" stroke=\"black\" stroke-width=\"1\" fill=\"#31D61E\"/> </svg>");
+                            svg.attr("id", "row_"+value[v].row+"_"+value[v].place);
+                            $("#room-scheme").append(svg);
 
 
-            $("circle").each(function () {
-                if($(this).attr('data-status')==="1") {
-                    $(this).addClass("enabled heyo top");
-                    jQuery(".top").tipso({
-                        titleContent: "",
-                        position: "top",
-                        color: "black",
-                        background: "#eee",
-                        width: "100",
-                        speed: 100
+                             $("#row_" + value[v].row + "_" + value[v].place).find('circle').attr({
+                                 "data-price": value[v].price,
+                                 "data-status": value[v].status,
+                                 "data-id": value[v].id,
+                                 "data-place": value[v].place,
+                                 "data-row": value[v].row,
+                                 "data-tipso": "Цена: " + value[v].price +"" + "\nМесто: "+value[v].place
+
+                             });
+
+                        }
+
+                        $("#room-scheme").append("<br/>");
+
                     });
-                }
-                else $(this).addClass("disabled");
-            });
-            $("circle").click(function () {
-                var priceOfAll = 0;
+                    $('.loader1').fadeOut();
 
-                if(!($(this).hasClass('disabled'))) {
-                    $(this).toggleClass("bought");
 
-                    var clicked = $(this).attr("data-status");
+                    $("circle").each(function () {
+                        if($(this).attr('data-status')==="1") {
+                            $(this).addClass("enabled heyo top");
+                            jQuery(".top").tipso({
+                                titleContent: "",
+                                position: "top",
+                                color: "black",
+                                background: "#eee",
+                                width: "100",
+                                speed: 100
+                            });
+                        }
+                        else $(this).addClass("disabled");
+                    });
+                    $("circle").click(function () {
+                        var priceOfAll = 0;
 
-                    if(clicked === "1") {
-                        $(this).attr("data-status", "0");
-                        //console.log("if work");
-                    }
-                    else {
-                        $(this).attr("data-status", "1");
-                        //console.log("else work")
-                    }
+                        if(!($(this).hasClass('disabled'))) {
+                            $(this).toggleClass("bought");
 
-                }
-                $("circle").each(function () {
-                    if($(this).hasClass("bought")){
-                        priceOfAll += parseInt($(this).attr("data-price"));
-                    }
-                })
+                            var clicked = $(this).attr("data-status");
 
-                $(".count").text(priceOfAll);
+                            if(clicked === "1") {
+                                $(this).attr("data-status", "0");
+                                //console.log("if work");
+                            }
+                            else {
+                                $(this).attr("data-status", "1");
+                                //console.log("else work")
+                            }
 
-            });
+                        }
+                        $("circle").each(function () {
+                            if($(this).hasClass("bought")){
+                                priceOfAll += parseInt($(this).attr("data-price"));
+                            }
+                        })
 
-        }).error(function () {
-            $("#hall-scheme").empty().css("text-align", "center").append("<h5 style='width: 100%'>Sorry hall is not found</h5>")
+                        $(".count").text(priceOfAll);
+
+                    });
+
+                }).error(function () {
+                    $("#hall-scheme").empty().css("text-align", "center").append("<h5 style='width: 100%'>Sorry hall is not found</h5>")
+                });
+
+            })
         });
+
+        // $.getJSON("../../room.json", function (data) {
+        //
+        //     if(data.info) {
+        //         $("#imgAction").attr("src", data.info.poster);
+        //
+        //         var dayAction = new Date(data.info.date);
+        //
+        //         var timeAction = dayAction.getHours();
+        //         var minuteAction = dayAction.getMinutes();
+        //         console.log(data.info.date);
+        //         console.log(timeAction + " : " + minuteAction);
+        //         $("#nameAction").text(data.info.title);
+        //         $("#whereAction").text(data.info.hall);
+        //         $("#timeAction").text(timeAction + " : " + minuteAction);
+        //     }
+        //
+        //     $.each(data, function (key, value) {
+        //         console.log(value.length);
+        //         $("#room-scheme").append("<span style='position: relative; top: -15px; right: 10px'>ryad: "+(key+1)+"</span>");
+        //         for(var v in value) {
+        //             //console.log(value[v].place + " in row " + value[v].row);
+        //
+        //             var svg = $("<svg height=\"20\" width=\"20\"> <circle cx=\"10\" cy=\"10\" r=\"5\" stroke=\"black\" stroke-width=\"1\" fill=\"#31D61E\"/> </svg>");
+        //             svg.attr("id", "row_"+value[v].row+"_"+value[v].place);
+        //             $("#room-scheme").append(svg);
+        //
+        //
+        //              $("#row_" + value[v].row + "_" + value[v].place).find('circle').attr({
+        //                  "data-price": value[v].price,
+        //                  "data-status": value[v].status,
+        //                  "data-id": value[v].id,
+        //                  "data-place": value[v].place,
+        //                  "data-row": value[v].row,
+        //                  "data-tipso": "Цена: " + value[v].price +"" + "\nМесто: "+value[v].place
+        //
+        //              });
+        //
+        //         }
+        //
+        //         $("#room-scheme").append("<br/>");
+        //
+        //     });
+        //     $('.loader1').fadeOut();
+        //
+        //
+        //     $("circle").each(function () {
+        //         if($(this).attr('data-status')==="1") {
+        //             $(this).addClass("enabled heyo top");
+        //             jQuery(".top").tipso({
+        //                 titleContent: "",
+        //                 position: "top",
+        //                 color: "black",
+        //                 background: "#eee",
+        //                 width: "100",
+        //                 speed: 100
+        //             });
+        //         }
+        //         else $(this).addClass("disabled");
+        //     });
+        //     $("circle").click(function () {
+        //         var priceOfAll = 0;
+        //
+        //         if(!($(this).hasClass('disabled'))) {
+        //             $(this).toggleClass("bought");
+        //
+        //             var clicked = $(this).attr("data-status");
+        //
+        //             if(clicked === "1") {
+        //                 $(this).attr("data-status", "0");
+        //                 //console.log("if work");
+        //             }
+        //             else {
+        //                 $(this).attr("data-status", "1");
+        //                 //console.log("else work")
+        //             }
+        //
+        //         }
+        //         $("circle").each(function () {
+        //             if($(this).hasClass("bought")){
+        //                 priceOfAll += parseInt($(this).attr("data-price"));
+        //             }
+        //         })
+        //
+        //         $(".count").text(priceOfAll);
+        //
+        //     });
+        //
+        // }).error(function () {
+        //     $("#hall-scheme").empty().css("text-align", "center").append("<h5 style='width: 100%'>Sorry hall is not found</h5>")
+        // });
 
     });
 
